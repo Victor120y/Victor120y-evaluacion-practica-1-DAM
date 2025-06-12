a3519befc07652645fae643f847b48c1b092876d
Alexis Miranda <mr22058@ues.edu.sv>
2025-06-12 08:03:14 -0600

    alinenado interfaz

--- app/src/main/java/com/example/veterinaria/dao/TareaDAO.java
+++ app/src/main/java/com/example/veterinaria/dao/TareaDAO.java
@@ -1,9 +1,34 @@
 package com.example.veterinaria.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
 import androidx.room.PrimaryKey;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.veterinaria.data.Tarea;
+
+import java.util.List;
 
 @Dao
 public interface TareaDAO {
 
+    @Insert
+    void crear(Tarea tarea);
+
+    @Update
+    void actualizar(Tarea tarea);
+
+    @Delete
+    void eliminar(Tarea tarea);
+
+    @Query("SELECT * FROM Tarea ")
+    List<Tarea> obtenerTodasLasTareas();
+
+   @Query("SELECT * FROM Tarea WHERE descripcion LIKE '%' || :desc || '%'")
+    List<Tarea> buscarTareasPorDescripcion(String desc);
+
+    @Query("SELECT * FROM Tarea WHERE nombre = :nombre AND descripcion = :descripcion")
+    Tarea buscarTareaPorNombreYDescripcion(String nombre, String descripcion);
 }
